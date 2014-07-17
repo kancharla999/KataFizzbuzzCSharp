4cd3cd5368c6b317d63899522e8bdd7e5406e214
mhelmore <matt@mhelmore.com>
2014-07-17 13:56:18 -0500

    Initial commit

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Answer(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/IFizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    interface IFizzBuzz
+    {    
+        /// <summary>
+        /// Give an answer to the current game
+        /// </summary>
+        /// <param name="number">current number in the game sequence</param>
+        /// <returns>appropriate answer to the current number</returns>
+        string Answer(int number);
+    }
+}
